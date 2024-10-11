240f230b8a174fd74b6129b328432984bdbfb2ae
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-10-11 14:21:31 -0300

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -3,8 +3,9 @@
 int main(int argc, char** argv){  
   
   std::cout 
-    << "Hello there! :^)" 
-    << '\n';  
+    << "Hello there! :^)\n"
+    << "I'm 17 years old\n"
+    << "And I love C/C++ and ASM\n";
   
     return 0;  
 }
